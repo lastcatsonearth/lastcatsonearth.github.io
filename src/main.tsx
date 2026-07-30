11d8b76108ab2d7b0b976924477e5f38880297c2
Antonio Coppola <antonio.coppola@tum.de>
2026-07-30 15:45:39 +0200

    restored

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,15 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+const query = new URLSearchParams(window.location.search);
+const redirect = query.get("p");
+
+if (redirect) {
+    window.history.replaceState(
+        null,
+        "",
+        redirect + window.location.hash
+    );
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
